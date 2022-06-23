b10b084ba2888e3e464942624a925a58879e3d74
David Lev <davidlev050414@gmail.com>
2022-06-23 23:29:22 +0300

    move random generator to helpers

--- meapi/helpers.py
+++ meapi/helpers.py
@@ -1,10 +1,14 @@
+import time
 from base64 import b64encode
 from datetime import datetime, date
 from quopri import encodestring
+from random import randint, choice, uniform, random
 from re import match, sub
 from typing import Union
 from requests import get
 from meapi.exceptions import MeException
+
+RANDOM_API = "https://random-data-api.com/api"
 
 
 def valid_phone_number(phone_number: Union[str, int]) -> int:
@@ -115,3 +119,53 @@ def get_vcard(data: dict, prefix_name: str = "", profile_picture: bool = True, *
     vcard_data['end'] = "END:VCARD"
 
     return "\n".join([val for val in vcard_data.values()])
+
+
+def random_date():
+    date_format = '%Y-%m-%dT%H:%M:%S%z'
+    stime = time.mktime(time.strptime('2020-05-12T00:00:11Z', date_format))
+    etime = time.mktime(time.strptime('2022-06-24T00:00:11Z', date_format))
+    ptime = stime + random() * (etime - stime)
+    return datetime.strptime(time.strftime(date_format, time.localtime(ptime)), date_format).strftime(date_format)
+
+
+def get_random_data(contacts=True, calls=True, location=True) -> dict:
+    if not contacts and not calls and not location:
+        raise MeException("You need to set True at least one of the random data types")
+
+    call_types = ['missed', 'outgoing', 'incoming']
+    random_data = {}
+
+    if contacts or calls:
+        count = randint(30, 50)
+        random_numbers = [phone['phone_number'] for phone in get(url=RANDOM_API+f'/phone_number/random_phone_number?size={count}"').json()]
+        random_names = [name['name'] for name in get(url=RANDOM_API+f'/name/random_name?size={count}').json()]
+
+        if contacts:
+            random_data['contacts'] = []
+            for contact in range(1, randint(10, 30)):
+                random_data['contacts'].append({
+                    "country_code": "XX",
+                    "date_of_birth": None,
+                    "name": str(choice(random_names)),
+                    "phone_number": int(sub(r'\D', '', str(choice(random_numbers))))
+                })
+
+        if calls:
+            random_data['calls'] = []
+            for call in range(1, randint(10, 30)):
+                random_data['calls'].append({
+                    "called_at": random_date(),
+                    "duration": randint(10, 300),
+                    "name": str(choice(random_names)),
+                    "phone_number": int(sub(r'\D', '', str(choice(random_numbers)))),
+                    "tag": None,
+                    "type": choice(call_types)
+                })
+
+    if location:
+        random_data['location'] = {}
+        random_data['location']['lat'] = - round(uniform(30, 60), 5)
+        random_data['location']['lon'] = round(uniform(30, 60), 5)
+
+    return random_data
